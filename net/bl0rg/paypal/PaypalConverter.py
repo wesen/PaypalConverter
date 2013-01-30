2cdbcdbd266bc5f72d632c9ffd6f6c8236f8de92
Manuel Odendahl <wesen@ruinwesen.com>
2013-01-30 22:02:11 +0100

    user friendliness

--- net/bl0rg/paypal/PaypalConverter.py
+++ net/bl0rg/paypal/PaypalConverter.py
@@ -7,11 +7,18 @@ Created on Dec 29, 2011
 
 import csv
 import codecs
+import cmd
+import os
 from operator import itemgetter, attrgetter
 import time
 import commands
 import re
 import cStringIO
+import readline
+import traceback
+import sys
+import email
+from email.header import decode_header
 
 def utf8_encoder(unicode_csv_data):
     for line in unicode_csv_data:
@@ -100,8 +107,8 @@ class Txn:
         return self.auszug.getReferringTxns(self.id)
     
     def __repr__(self):
-        return "<Txn: %s (%s) \"%s\" %s %s - %s>" % (self.getTypeCharacter(), self.id, self.name, self.waehrung, self.brutto, self.description)
-    
+        return "<Txn: %s (%s - \"%s\") \"%s\" %s %s - %s>" % (self.getTypeCharacter(), self.id, self.art, self.name, self.waehrung, self.brutto, self.description)
+
     def getTypeCharacter(self):
         if self.isPaypalTxn():
             return "P"
@@ -192,36 +199,126 @@ class Txn:
         return row
     
     def findSimilarEmails(self):
-        _created = "created:%s" % (time.strftime("%d/%m/%Y", self.date))
+        _time = time.strftime("%d/%m/%Y", self.date)
+        _created = "created:%s" % _time
         def find(query):
-            return "mdfind -interpret '%s %s kind:mail'" % (_created, query)
-        
-        _cmd = ";".join([find(x) for x in [self.name, self.receiver]]) 
-        print _cmd
-        _file = commands.getstatusoutput(_cmd)[1]
+            query = query.encode('ascii', 'ignore')
+#            print "searching for \"%s\" on %s" % (query,_time)
+            return "mdfind -interpret '%s \"%s\" kind:mail'" % (_created, query)
+
+        if self.isConversion() or self.isAuthorization():
+            return []
+
+        ignores = [ "von Euro", "in Euro", "von US-Dollar", "in US-Dollar", "Kreditkarte", "Bankkonto (Lastschrift)"]
+        _cmd = ";".join([find(x) for x in [self.name, self.receiver] if x not in ignores])
+        if _cmd == "":
+            return []
+#        print _cmd.encode('ascii', 'ignore')
+        _file = commands.getstatusoutput(_cmd.encode('ascii', 'ignore'))[1]
         files = _file.split("\n")
         _email = self.findEmail()
         if _email and files.count(_email) == 0:
             files.append(_email)
 #        if files.count(_email) > 0:
 #            files.remove(_email)
-        return files
+        return [ f for f in files if f ]
     
     def openSimilarEmails(self):
         for email in self.findSimilarEmails():
             commands.getstatusoutput("open \"%s\"" % email)
     
     def findEmail(self):
-#        _cmd = "mdfind -interpret '%s' | grep IMAP | head -1" % self.id
-        _cmd = "/usr/local/bin/sense -n 0.8 'mdfind -literal \"kMDItemTextContent = *%s && kMDItemContentType=com.apple.mail.elmx \"'" % self.id
-        print _cmd
+        if self.isConversion() or self.isAuthorization():
+            return None
+
+#        print "searching for id: %s" % self.id
+        _cmd = "/usr/local/bin/sense -n 0.8 'mdfind -interpret \"\"%s\" kind:mail\"'" % self.id
+#        print _cmd.encode('ascii', 'ignore')
         _file = commands.getstatusoutput(_cmd)[1]
         if _file != '':
-            print "found %s" % _file
+#            print "found %s" % _file
             return _file
         else:
             return None
-    
+
+    def parseKmdOutput(self, path):
+        "Run Spotlight 'mdls' command on a file and return result as a Python dict."
+        # needs special handling of date fields like "2007-05-24 13:08:05 +0200"
+
+        # run 'mdls' command, grab its output and remove newlines
+        output = commands.getoutput(str("mdls " + path))
+        s = output.replace('\n', '')
+
+        # split the string on 'kMD' into lines
+        kmdLines = s.split('kMD')
+        kmdLines = [line for line in kmdLines if line]
+        # print "** kmdLines"
+        # pp(kmdLines)
+        items = [re.match("(\w+) *= *(.*)", line) for line in kmdLines]
+        items = [m.groups() for m in items if m]
+        # print "** items"
+        # pp(items)
+
+        # reinsert kMD
+        items = [("kMD" + k, v) for (k, v) in items]
+        # print "** items"
+        # pp(items)
+
+        # work around a feature in Mac OS X
+        # (which puts no quotes around 'simple' strings)
+        pairs = []
+        for item in items:
+            try:
+                k, v = item
+            except:
+                # print "### item", item
+                # print path
+                raise
+            v = v.strip()
+            try:
+                ev = eval(v)
+            except:
+                # print "### failed evaluating", repr(v)
+                if v[0] not in ('"', "'"):
+                    v = v.replace('"', '')
+                    v = v.replace("'", '')
+                if v.startswith('('): # and v.startswith(')'):
+                    # print "---", v
+                    v = re.sub("(\w+)", lambda m:'"%s"' % m.groups()[0], v)
+                    # also turn a single name into a one element tuple
+                    # print "****", repr(v)
+                    # if type(eval(v)) == str:
+                    #     v = "(%s,)" % v
+                # print "***", repr(v)
+            try:
+                pairs.append([k, eval(v)])
+            except SyntaxError:
+                pairs.append([k, v])
+
+        data = dict(pairs)
+
+        return data
+
+    def getEmailInfo(self, info):
+        f = open(info)
+        data = f.read()
+        data = data.split('\n', 1)[1]
+        msg = email.message_from_string(data)
+        _from = decode_header(msg.get("From"))
+        if len(_from) > 0:
+            _from = _from[0][0]
+        subject = decode_header(msg.get("Subject"))
+        if len(subject) > 0:
+            subject= subject[0][0]
+        if _from and subject:
+            return "Subject: %s from: %s" % (subject, _from)
+#        data = self.parseKmdOutput(info)
+#        print "email %s info: %s" % (info, len(data.keys()))
+#        if len(data) > 0:
+#            return "Subject: %s from: %s" % (data.get("kMDItemDisplayName", ""), data.get("kMDItemAuthors", "Unknown"))
+        else:
+            return None
+
     def openEmail(self):
         email = self.findEmail()
         if email:
@@ -274,7 +371,7 @@ class Auszug:
     def openEmails(self):
         for txn in self.sortedTxns():
             if txn.findEmail() and (not txn.isPrivate()):
-                print txn
+                print txn.__repr__().encode('ascii', 'ignore')
                 txn.openEmail()
                 raw_input("Press Enter to continue")
     
@@ -293,29 +390,152 @@ class Auszug:
                 if row["brutto"] != "":
                     writer.writerow([row[x] for x in ["date", "name", "brutto", "original", "gebuehr", "guthaben", "notes"]])
 
-auszug = Auszug.readCSV("/Users/manuel/Downloads/Herunterladen.csv")
-auszug.printCSV("/tmp/out.csv")
-print "printed CSV out to /tmp/out.csv\n"
-for txn in auszug.sortedTxns():
-    email = txn.findEmail()
-    emails = txn.findSimilarEmails()
-    print "%s: %s, %s\n" % (txn.id, email, emails)
+def intTryParse(value):
+    try:
+        return int(value)
+    except ValueError:
+        return None
 
-def openSpotlight(query):
-    applescript = '''on run argv # we expect program arguments
-    tell application "Finder"
-        activate # focus Finder
-        tell application "System Events"
-            keystroke "f" using command down # press Cmd-F
-            keystroke (item 1 of argv) # enter the program argument into search box
-            key code 36 # press enter
-            key code 48 # press tab
-            keystroke " " # press space
-        end tell
-    end tell
-end run
-'''
-    _cmd = "osascript " + " ".join([ "-e '%s'" % x for x in applescript.split('\n')]) + " " + query
-    commands.getstatusoutput(_cmd)
-    
+class PaypalConsole(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = "paypal> "
+        self.intro = "Welcome to the Paypal Converter"
+        self.auszug = None
+        self.do_csv("~/Downloads/Herunterladen.csv")
+        readline.set_completer_delims(' \t\n`@#$%^&*()-=+[{]}\\|;:\'",<>?')
 
+    def do_csv(self, name):
+        try:
+            self.auszug = Auszug.readCSV(os.path.expanduser(name))
+        except:
+            traceback.print_exc(file=sys.stdout)
+            print "Error parsing CSV"
+
+    def do_out(self, name):
+        if not self.auszug:
+            print "please first parse a csv"
+            return
+        try:
+            self.auszug.printCSV(name)
+            print "written output csv to " + name
+        except:
+            traceback.print_exc(file=sys.stdout)
+            print "error while writing csv"
+
+    def do_emails(self, args):
+        if not self.auszug:
+            print "please first parse a csv"
+            return
+        try:
+            for txn in self.auszug.sortedTxns():
+                if txn.isConversion() or txn.isAuthorization():
+                    continue
+                if txn.name in ["Kreditkarte", "Bankkonto (Lastschrift)"]:
+                    continue
+                print txn.__repr__().encode('ascii', 'ignore')
+                emails = txn.findSimilarEmails()
+#                print "similar %s" % emails
+                i = 0
+                for f in emails:
+                    i += 1
+                    info = txn.getEmailInfo(f)
+                    if info:
+                        print "%s) %s" % (i, info)
+                if len(emails) > 0:
+                    _open = raw_input("Open email: ")
+                    idx = intTryParse(_open)
+                    if idx is not None and 0 < idx <= len(emails):
+                        commands.getstatusoutput("open \"%s\"" % emails[idx - 1])
+                print "\n"
+        except Exception as e:
+            print "error while searching for emails"
+            traceback.print_exc(file=sys.stdout)
+            print e
+
+    def list_files(self, start, ends = None):
+        res = []
+        _real = os.path.expanduser(start)
+        if os.path.isfile(_real):
+            return [start]
+
+        _dir = os.path.dirname(_real)
+        if _dir == "":
+            _dir = "."
+            _base = ""
+        else:
+            _base = os.path.basename(_real)
+
+        dir = os.path.dirname(start)
+#        print dir
+        if not (dir == "") and not dir.startswith(os.path.sep):
+            dir += os.path.sep
+
+#        print "_dir " + _dir + " dir " + dir
+
+        for f in  os.listdir(_dir):
+            full = _dir + os.path.sep + f
+#            print full
+#            print f.startswith(_base)
+#            print _base
+            if (os.path.isfile(full) and
+                f.startswith(_base) and
+                (not ends or f.endswith(ends))
+                and (f != _base)):
+#                print "adding f " + f + " base " + _base
+                res.append(dir + f)
+            elif os.path.isdir(full) and f.startswith(_base):
+                res.append(dir + f + os.path.sep)
+#        print "\n"
+#        print  res
+#        print "\n"
+        return res
+
+    def complete_csv(self, text, line, begidx, endidx):
+        mline = line.split(' ', 1)[1]
+#        print "mline " + mline
+        if not mline or mline == "":
+            return self.list_files(".", ".csv")
+        else:
+            return self.list_files(mline, ".csv")
+
+    def help_csv(self):
+        print '\n'.join(
+            ["load csv",
+            "Parse a paypal csv dump"])
+
+    def do_EOF(self, args):
+        return self.do_exit(args)
+
+    def do_exit(self, args):
+        return -1
+
+if __name__ == '__main__':
+    PaypalConsole().cmdloop()
+
+#auszug = Auszug.readCSV("/Users/manuel/Downloads/Herunterladen.csv")
+#auszug.printCSV("/tmp/out.csv")
+#print "printed CSV out to /tmp/out.csv\n"
+#for txn in auszug.sortedTxns():
+#    email = txn.findEmail()
+#    emails = txn.findSimilarEmails()
+#    print "%s: %s, %s\n" % (txn.id, email, emails)
+#
+#def openSpotlight(query):
+#    applescript = '''on run argv # we expect program arguments
+#    tell application "Finder"
+#        activate # focus Finder
+#        tell application "System Events"
+#            keystroke "f" using command down # press Cmd-F
+#            keystroke (item 1 of argv) # enter the program argument into search box
+#            key code 36 # press enter
+#            key code 48 # press tab
+#            keystroke " " # press space
+#        end tell
+#    end tell
+#end run
+#'''
+#    _cmd = "osascript " + " ".join([ "-e '%s'" % x for x in applescript.split('\n')]) + " " + query
+#    commands.getstatusoutput(_cmd)
+#
+#
